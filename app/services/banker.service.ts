bea5b90ff8345709f42c2953bfd388ddcf0d6c2c
zerbusdetroy <github@zerbus.fr>
2016-03-11 11:14:53 +0100

    Correct banker login handle error

--- app/services/banker.service.ts
+++ app/services/banker.service.ts
@@ -76,7 +76,7 @@ export class BankerService {
 		// in a real world app, we may send the error to some remote logging infrastructure
 		// instead of just logging it to the console
 		console.error(error);
-		return Observable.throw(error.json().error || 'Server error');
+		return Observable.throw(error.text() || 'Server error');
 	}
 
 }
